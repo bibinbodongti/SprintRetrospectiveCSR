b6408451cfe89e82f856848a55eb93e6a8553317
bibinbodongti <phamdangdangkhoa2802@gmail.com>
2020-11-21 13:58:13 +0700

    Updates

---
+++ precache-manifest.200901bc21d6870f5b3a0ed121b259da.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "acd902cabee360f757a1e12e9f149889",
+    "url": "/Test/index.html"
+  },
+  {
+    "revision": "75da8615d7ee5026d8a6",
+    "url": "/Test/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "e2b91d979511dfca61f7",
+    "url": "/Test/static/css/main.b960d665.chunk.css"
+  },
+  {
+    "revision": "75da8615d7ee5026d8a6",
+    "url": "/Test/static/js/2.24ac25c7.chunk.js"
+  },
+  {
+    "revision": "d0693b680b8f14c85fc2a1a3cd7f5933",
+    "url": "/Test/static/js/2.24ac25c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2b91d979511dfca61f7",
+    "url": "/Test/static/js/main.3c5b813e.chunk.js"
+  },
+  {
+    "revision": "5748086ef9085e3b758a",
+    "url": "/Test/static/js/runtime-main.d83bdd70.js"
+  }
+]);
